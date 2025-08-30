a9250e72affa285a1889979d882220193552fda4
gene <eugenefedchenco@gmail.com>
2025-08-30 20:40:41 +0300

    Fix ApplicationList props

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <Block className={style.block}>
             <h1 className={style.title}>Simple apps to make your life easier</h1>
             <p className={style.note}>Free, online, secure, no account required.</p>
-            <ApplicationList items={list} min={10} />
+            <ApplicationList items={list} />
             <Loaded />
         </Block>
     );
