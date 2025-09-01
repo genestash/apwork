67a840a319414179f52d585b12c581f36852f721
gene <eugenefedchenco@gmail.com>
2025-09-01 10:11:58 +0300

    Add canonical to legal pages

--- nextjs/src/app/contact/page.tsx
+++ nextjs/src/app/contact/page.tsx
@@ -4,8 +4,11 @@ import style from './style.module.css';
 
 export const metadata = {
     title: 'Contact Us',
-    description:
-        'Get in touch with Apwork for questions, suggestions, or project collaboration. Contact us easily via email at contact@apwork.co'
+    description: 'Get in touch with Apwork for questions, suggestions, or project collaboration. Contact us easily via email at contact@apwork.co',
+
+    alternates: {
+        canonical: '/contact'
+    }
 };
 
 export default function Page() {
@@ -13,9 +16,8 @@ export default function Page() {
         <Block className={style.document}>
             <h1>Contact Us</h1>
             <p>
-                If you have any questions, suggestions, or would like to help the project, simply contact us directly at
-                contact@apwork.co. We do our best to respond to all messages within a few business days. Response times may be
-                longer during weekends or holidays.
+                If you have any questions, suggestions, or would like to help the project, simply contact us directly at contact@apwork.co. We do our best to respond to all messages within a few business days. Response times may be longer during weekends
+                or holidays.
             </p>
             <Loaded />
         </Block>
